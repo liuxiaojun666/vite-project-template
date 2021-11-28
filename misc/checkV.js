5965ae14627f3a8ba875e5fa2071b397e27ca810
liuxiaojun <lxj_web@163.com>
2021-11-28 13:47:29 +0800

    node 版本控制

--- misc/checkV.js
+++ misc/checkV.js
@@ -1,7 +1,7 @@
 const { engines } = require('../package')
 
 const version = engines.node
-if (process.version.substr(1).split('.')[0] !== engines.node) {
+if (process.version.substr(1).split('.')[0] !== engines.node.split('.')[0]) {
   console.error(`Required node version ${version}, got: ${process.version}.`)
   process.exit(1)
 }
