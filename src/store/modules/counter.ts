1d863bd8183693bfbac4477516cb0c0b9dc26d34
liuxiaojun <lxj_web@163.com>
2021-11-29 11:42:23 +0800

    store update

--- src/store/modules/counter.ts
+++ src/store/modules/counter.ts
@@ -1,7 +1,7 @@
 import { defineStore } from 'pinia'
-import { USER_STORE } from '../types'
+import { COUNTER_STORE } from '../types'
 
-const useStore = defineStore(USER_STORE, {
+const useStore = defineStore(COUNTER_STORE, {
   state: () => {
     return {
       counter: 0
--- src/store/types.ts
+++ src/store/types.ts
@@ -1,1 +1,2 @@
 export const USER_STORE = 'user_store'
+export const COUNTER_STORE = 'counter_store'
