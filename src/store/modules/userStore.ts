9d650e27dcc707578943a0ad0475c210c6daa007
liuxiaojun <lxj_web@163.com>
2021-11-29 20:09:02 +0800

    update

--- src/store/modules/userStore.ts
+++ src/store/modules/userStore.ts
@@ -1,5 +1,5 @@
 import { ILoginRequestData } from '@/api/user.type'
-// import { login } from '@/api/user'
+import { login } from '@/api/user'
 import { addAsyncRoutes } from '@/router/permisstion'
 import { defineStore } from 'pinia'
 import { USER_STORE } from '../types'
@@ -14,10 +14,14 @@ const useUserStore = defineStore(USER_STORE, {
   },
   actions: {
     async login (data: ILoginRequestData) {
-      // const [err, res] = await login(data)
-      // if (err) return console.error(err)
-      // this.token = res.data.token
-      this.token = 'ok'
+      const ceshi = true // 使用时删除测试代码
+      if (!ceshi) {
+        const [err, res] = await login(data)
+        if (err) return console.error(err)
+        this.token = res.data.token
+      } else {
+        this.token = 'ok'
+      }
       addAsyncRoutes()
       return true
     },
